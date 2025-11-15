4d9865ccf67c70493d273f24a7da6b1e6e6a5e60
Katie Park <147112323+KJP952@users.noreply.github.com>
2025-11-14 23:28:00 -0500

    Updated More information panel and link to rulesheet

--- frontend/src/components/More.jsx
+++ frontend/src/components/More.jsx
@@ -1,11 +1,17 @@
+import '../component_styles/More.css';
+
 function More() {
   return (
     <div className = "outer-container">
       <div className = "middle-container">
 
-          <div className='points-container'>
             <p className='point-value'> Learn More </p>
-        </div>
+            <p className="game-summary">This site tracks the Emory Women’s Golf point system - players earn points from training and team actives. Milestones unlock rewards (prize) and a season goal </p>
+            <a target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+              href="https://docs.google.com/document/d/1pz1OdZDN2u_cPtey3aIq00W0XO1I0jaTAiYnIDFseB8/edit?usp=sharing">Click Here for Rules</a>
+
 
       </div>
     </div>
