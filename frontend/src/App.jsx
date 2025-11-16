0910de3c46ee7ae6024a78ae5c17483de3736e74
Zimo Li <zli938@emory.edu>
2025-11-16 00:24:45 -0500

    handled logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,8 +17,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const [user, setUser] = useState(() => {
-
-  const savedUser = localStorage.getItem("user");
+    const savedUser = localStorage.getItem("user");
     const loginTime = localStorage.getItem("loginTime");
 
     if (!savedUser || !loginTime) return null;
@@ -44,7 +43,7 @@ function App() {
 
       const now = Date.now();
       const THIRTY_MIN = 30 * 60 * 1000;
-      
+
       if (now - loginTime > THIRTY_MIN) {
         handleLogout();
       }
@@ -55,24 +54,21 @@ function App() {
 
   function handleLogout() {
     setUser(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("loginTime");
   }
 
   return (
-     <Router>
-      <Header user={user} onLogout={() => setUser(null)} />
+    <Router>
+      <Header user={user} onLogout={handleLogout} />
       <Routes>
+        <Route path="/" element={<Home user={user} />} />
+        <Route path="/login" element={<AuthPage onLogin={setUser} />} />
 
-      <Route path="/" element={<Home user={user} />} />
-      <Route path="/login" element={<AuthPage onLogin={setUser} />} />
-
-      <Route
-        path="/activities"
-        element={
-          <ProtectedRoute user={user}>
-          </ProtectedRoute>
-        }
+        <Route
+          path="/activities"
+          element={<ProtectedRoute user={user}></ProtectedRoute>}
         />
-
       </Routes>
     </Router>
   );
